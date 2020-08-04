5fa7bd9b61c6ffb29909c05e99942bdfb838cac7
Nico Hinze <hinze.nico@t-online.de>
2020-08-05 00:13:19 +0200

    Add unit test for ray

---
+++ tests/ray_utest.cpp
@@ -0,0 +1,26 @@
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "catch.hpp"
+#include <glm/glm.hpp>
+
+#include "ray.hpp"
+
+TEST_CASE("Ray reflection", "[ray]") {
+    auto ray = Ray(glm::vec3(0, 0, 0), glm::vec3(1, 1, 0));
+    auto n = glm::vec3(-1, 0, 0);
+    auto expected = glm::vec3(-1, 1, 0);
+    auto reflect = ray.reflect(n);
+    REQUIRE(reflect == expected);
+
+    ray = Ray(glm::vec3(0, 0, 0), glm::vec3(0, 0, 0));
+    n = glm::vec3(-1, 0, 0);
+    expected = glm::vec3(0, 0, 0);
+    reflect = ray.reflect(n);
+    REQUIRE(reflect == expected);
+
+    ray = Ray(glm::vec3(0, 0, 0), glm::vec3(1, 1, 0));
+    n = glm::vec3(0, 0, 0);
+    expected = glm::vec3(1, 1, 0);
+    reflect = ray.reflect(n);
+    REQUIRE(reflect == expected);
+}
